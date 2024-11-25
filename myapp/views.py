e396b589c88bcbea8c29e88981e7c695712d4bb9
Adrien Caplat <acaplat@k2r1p10.42mulhouse.fr>
2024-11-25 20:15:47 +0100

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'myapp/home.html')
+
+def page1(request):
+    return render(request, 'myapp/page1.html')
+
+def page2(request):
+    return render(request, 'myapp/page2.html')
+
