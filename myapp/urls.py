dc9eb2e44140137720a1b54d9c010fbc649e8e40
Adrien Caplat <acaplat@k2r1p7.42mulhouse.fr>
2025-01-13 18:27:12 +0100

    ajout barre deroulante user

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_redirect, name="home_redirect"),
+    # path('', views.home_redirect, name="home_redirect"),
+    path('', views.index, name="index"), 
     path('home/', views.home, name='home'),
     path('page1/', views.page1, name='page1'),
     path('page2/', views.page2, name='page2'),
